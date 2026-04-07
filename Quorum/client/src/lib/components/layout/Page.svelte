a94d4b0c9a16f5a114043b9ce6af613ae311b655
jamster3000 <jrbaines.04@gmail.com>
2026-04-07 22:44:50 +0100

    Add environment config and database setup

---
+++ Quorum/client/src/lib/components/layout/Page.svelte
@@ -0,0 +1,12 @@
+<div class="page">
+	<slot/>
+</div>
+
+<style>
+	.page {
+		min-height: 100vh;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+</style>
--- Quorum/server/Quorum-backend/src/main.rs
+++ Quorum/server/Quorum-backend/src/main.rs
@@ -20,12 +20,40 @@ struct EchoResponse {
 	echoed: String,
 }
 
+#[derive(Deserialize)]
+struct SearchRequest {
+	query: String,
+}
+
+#[derive(Serialize)]
+struct SearchResponse {
+	query: String,
+	results: String,
+}
+
+async fn search(Json(payload): Json<SearchRequest>) -> Json<SearchResponse> {
+	let results;
+	let userQuery = payload.query.to_lowercase();
+
+	if (userQuery == "test") {
+		results = "If you are seeing this message, it works".to_string();
+	} else {
+		results = "No results found".to_string();
+	}
+
+	Json(SearchResponse {
+		query: payload.query,
+		results,
+	})
+}
+
 #[tokio::main]
 async fn main() {
 	let app = Router::new()
 	    .route("/", get(root))
 		.route("/health", get(health))
-		.route("/echo", post(echo));
+		.route("/echo", post(echo))
+		.route("/search", post(search));
 
 	let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
 	let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
