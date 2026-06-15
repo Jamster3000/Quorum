7713f458eb91813100dbcb2d25589646b155274f
Chilling with Gazoz <kerimdemirbay21@gmail.com>
2026-06-15 16:34:02 +0300

    Added a basic level of CORS

--- server/src/routes/route.rs
+++ server/src/routes/route.rs
@@ -16,6 +16,12 @@ use super::auth::{
 use super::echo::echo;
 use super::health::health;
 
+// Necessary for CORS + Security headers
+use tower_http::cors::{CorsLayer, Any};
+use tower_http::set_header::SetResponseHeaderLayer;
+use http::header::{HeaderName, HeaderValue};
+use tower::ServiceBuilder;
+
 /// Creates the router with all the defined routes and their handlers.
 /// Includes request limiting to avoid getting too many requests at once.
 ///
@@ -25,11 +31,33 @@ use super::health::health;
 /// # Returns
 /// A configured `Router` instance with all routes and their handlers.
 pub fn create_router(db: DB) -> Router {
+
+    let cors = CorsLayer::new()
+        .allow_origin(Any) // replace with more specific origin for added safety
+        .allow_methods(Any)
+        .allow_headers(Any);
+
+        let security_headers = ServiceBuilder::new()
+            .layer(SetResponseHeaderLayer::if_not_present(
+                HeaderName::from_static("x-frame-options"),
+                HeaderValue::from_static("DENY"),
+            ))
+            .layer(SetResponseHeaderLayer::if_not_present(
+                HeaderName::from_static("strict-transport-security"),
+                HeaderValue::from_static("max-age=31536000; includeSubDomains; preload"),
+            ))
+            .layer(SetResponseHeaderLayer::if_not_present(
+                HeaderName::from_static("x-content-type-options"),
+                HeaderValue::from_static("nosniff"),
+            ));
+
     Router::new()
         .route("/", get(|| async { "Axum server is running" }))
         .route("/health", get(health))
         .route("/echo", post(echo))
         .merge(auth_routes())
+        .layer(cors) //can be replaced with .allow_origin("http://localhost:xxxx".parse::<HeaderValue>().unwrap()) for more specific origin safety
+        .layer(security_headers)
         .with_state(db)
 }
 
