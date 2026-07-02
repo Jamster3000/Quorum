b406d008a1cc2d580d17bf43c5eef96c486dc710
jamster3000 <jrbaines.04@gmail.com>
2026-07-02 22:51:14 +0100

    Private server hasn't been checked but removed instances for docker and dontenvy crate.

--- server/quorum-private/src/main.rs
+++ server/quorum-private/src/main.rs
@@ -10,7 +10,6 @@ mod utility;
 use colored::Colorize;
 //use reqwest::Client;
 use crate::db::schema;
-use crate::utility::docker;
 use quorum_core::cli;
 use quorum_core::db as core_db;
 use quorum_core::startup;
@@ -39,12 +38,6 @@ async fn main() {
         }
     };
 
-    startup::print_step(
-        "Loading environment",
-        true,
-        startup::elapsed(startup::create_timer()),
-    );
-
     // Load the config once and reuse it
     let config = quorum_core::utility::config::Config::get();
 
@@ -52,43 +45,13 @@ async fn main() {
     let timer = startup::create_timer();
     let _db = match core_db::init().await {
         Ok(db) => {
-            startup::print_step("Connecting to database", true, startup::elapsed(timer));
+            startup::print_step("Opening database", true, startup::elapsed(timer));
             db
         }
-        Err(_) => {
-            let docker_timer = startup::create_timer();
-            match docker::ensure_containers_running().await {
-                Ok(()) => {
-                    startup::print_step("Starting Docker", true, startup::elapsed(docker_timer));
-
-                    let retry_timer = startup::create_timer();
-                    match core_db::init().await {
-                        Ok(db) => {
-                            startup::print_step(
-                                "Connecting to database",
-                                true,
-                                startup::elapsed(retry_timer),
-                            );
-                            db
-                        }
-                        Err(e) => {
-                            startup::print_step(
-                                "Connecting to database",
-                                false,
-                                startup::elapsed(retry_timer),
-                            );
-                            eprintln!("{}", format!("  Error: {}", e).red());
-                            std::process::exit(1);
-                        }
-                    }
-                }
-                Err(docker_err) => {
-                    startup::print_step("Connecting to database", false, startup::elapsed(timer));
-                    startup::print_step("Starting Docker", false, startup::elapsed(docker_timer));
-                    eprintln!("{}", format!("  Error: {}", docker_err).red());
-                    std::process::exit(1);
-                }
-            }
+        Err(e) => {
+            startup::print_step("Opening database", false, startup::elapsed(timer));
+            eprintln!("{}", format!("  Error: {}", e).red());
+            std::process::exit(1);
         }
     };
 
