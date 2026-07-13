f06351c78af9bb09e73396f215a5ee074585e659
jamster3000 <jrbaines.04@gmail.com>
2026-07-13 22:25:25 +0100

    Ran fmt and satisfied clippy

--- client/src-tauri/src/config.rs
+++ client/src-tauri/src/config.rs
@@ -3,10 +3,7 @@ use std::sync::OnceLock;
 static SERVER_URL: OnceLock<String> = OnceLock::new();
 
 pub fn init() {
-	dotenvy::dotenv().ok();
-
-    let url = std::env::var("SERVER_URL")
-        .unwrap_or_else(|_| "http://127.0.0.1:3000".to_string());
+    let url = "http://127.0.0.1:3000".to_string();
 
     SERVER_URL.set(url).expect("Failed to set SERVER_URL");
 }
@@ -14,9 +11,5 @@ pub fn init() {
 /// Gets the url of the public SERVER_URL
 ///
 pub fn get_server_url() -> &'static str {
-    SERVER_URL.get_or_init(|| {
-        dotenvy::dotenv().ok();
-        std::env::var("SERVER_URL")
-            .unwrap_or_else(|_| "http://127.0.0.1:3000".to_string())
-    })
+    "http://127.0.0.1:3000"
 }
