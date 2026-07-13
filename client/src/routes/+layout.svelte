ad18080028a1bd1238ac9681140e4c64b0e3c239
jamster3000 <jrbaines.04@gmail.com>
2026-07-13 19:08:36 +0100

    Fixed a potential minor memory leak where `Store.load()` was being called every 10 minutes

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -4,21 +4,14 @@
   import { navigating } from '$app/stores';
   import { onMount } from 'svelte';
   import { isTokenValid } from '$lib/utils/auth';
-  import { Store } from '@tauri-apps/plugin-store';
 
   onMount(() => {
       const interval = setInterval(async () => {
-          const tokenValid = await isTokenValid();
-
-          if (tokenValid) {
-              const store = await Store.load('.auth.dat');
-              const accessToken = await store.get('access_token');
-          }
-      }, (10 * 60 * 1000)); // Check every 10 minutes
+          await isTokenValid();
+      }, 10 * 60 * 1000);
 
       return () => clearInterval(interval);
   });
-
 </script>
 
 {#key $navigating?.to?.url.pathname}
