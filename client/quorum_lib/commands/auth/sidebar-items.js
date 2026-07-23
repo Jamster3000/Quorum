69295c58f1dfffeb2c9e9994d6a68320d953dffe
KerimDemirbay <KerimDemirbay@users.noreply.github.com>
2026-07-23 16:51:54 +0000

    deploy: 52c30370de5e911b77617ddb7ad1f0b5f4321e0d

--- client/quorum_lib/commands/auth/sidebar-items.js
+++ client/quorum_lib/commands/auth/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["login","refresh_token","signup"],"struct":["LoginPayload","LoginResponse","SignupPayload","SignupResponse"]};
+window.SIDEBAR_ITEMS = {"fn":["login","refresh_token","signup"],"struct":["AuthSuccess","LoginPayload","SignupPayload","SignupSuccess"]};
