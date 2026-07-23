e91e8881ff198ca88393e6cfd101f6501efb1a31
KerimDemirbay <kerimdem153@gmail.com>
2026-07-23 16:38:22 +0100

    Added Tauri plug-in to ensure only one instance is running at any given time

--- client/src-tauri/src/lib.rs
+++ client/src-tauri/src/lib.rs
@@ -7,6 +7,15 @@ use tauri_plugin_store::Builder as StoreBuilder;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .setup(|app| {
+            #[cfg(desktop)]
+            app.handle()
+            .plugin(
+                tauri_plugin_single_instance::init(|_app, _args, _cwd| {})
+            )?;
+
+            Ok(())
+        })
         .plugin(tauri_plugin_opener::init())
         .plugin(StoreBuilder::default().build())
         .invoke_handler(tauri::generate_handler![
