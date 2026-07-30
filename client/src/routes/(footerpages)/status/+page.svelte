3c9ffd1d1d1b192f3bdbfc1c7a7653f12999cb07
jamster3000 <jrbaines.04@gmail.com>
2026-07-30 14:25:04 +0100

    Added content to a status page

--- client/src/routes/(footerpages)/status/+page.svelte
+++ client/src/routes/(footerpages)/status/+page.svelte
@@ -2,13 +2,17 @@
   import Header from '$lib/components/app/Header.svelte';
   import Footer from '$lib/components/app/Footer.svelte';
   import Auth from '$lib/utils/Auth.svelte';
+  import Card from '$lib/components/ui/Card.svelte';
 </script>
 
 <Auth redirectTo="/home" shouldBeLoggedIn={false} />
 <Header />
 
 <main class="page">
-	<p> Successfull connection to status</p>
+	<Card variant="gradient">
+		<h1>Unavailable</h1>
+		<p>Due to the lack of server hosting - there is no server uptime status to view.</p>
+	</Card>
 </main>
 
 <Footer />
